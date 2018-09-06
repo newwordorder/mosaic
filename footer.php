1b3c2ac7fe0366130f38a25754465d2f8a91c9a3
Nicholas MacArthur <n.j.macarthur@gmail.com>
2018-09-06 11:27:33 +1000

    Created Header and  Began footer

--- footer.php
+++ footer.php
@@ -9,26 +9,39 @@
 
 ?>
 
-<footer id="footer">
-		<div class="container text-center">
+<section id="pre-footer">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-sm-8">
+					<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+<footer id="footer" style=" background-image:url('<?php bloginfo('template_directory'); ?>/img/footer_bg.svg');" >
+		<div class="container">
 
 				<div class="row justify-content-center">
 
 						<div class="col-sm-8">
-								<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</h1>
+								<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor</h3>
+						</div>
+						<div class="col-sm-4">
+							<a class="btn btn-primary">Get in Touch</a>
 						</div>
 				</div>
 
 				<div class="row">
 						<div class="col-sm-12">
 							<a href="<?php echo get_home_url(); ?>" id="" class="footer__logo">
-								<img class="footer__logo" src="<?php bloginfo('template_directory'); ?>/img/logo--white.svg" alt="Logo">
+								<img class="footer__logo" src="<?php bloginfo('template_directory'); ?>/img/logo_white.svg" alt="Logo">
 							</a>
 						</div>
 				</div>
 				<div class="row">
 						<div class="col-sm-12">
-								<p>© Copyright New Life Wynyard | <a href="#">Privacy Policy</a> | <a href="#">Site by Rouleur</a> </p>
+								<p>© Copyright Mosaic | <a href="#">Privacy Policy</a> | <a href="#">Site by NWO.Digital</a> </p>
 						</div>
 						<div class="col-sm-3">
 
